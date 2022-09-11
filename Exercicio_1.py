6648f8f1135641794d969f5100319098647d129f
nascimentodorafael <107429969+nascimentodorafael@users.noreply.github.com>
2022-09-11 18:39:33 -0300

    Add files via upload

---
+++ Exercicio_1.py
@@ -0,0 +1,8 @@
+L = [1, 2, 3, 4, 5] 
+  
+  
+count = sum(L) 
+avg = count/len(L) 
+  
+print("sum = ", count) 
+print("average = ", avg)
---
+++ Exercicio_2.py
@@ -0,0 +1,6 @@
+senha = input('Digite sua senha: ')
+if senha == 'Senha123@':
+
+      print('Você está logado')
+else:
+      print('Senha incorreta')
